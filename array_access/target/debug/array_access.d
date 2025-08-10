51e2def60d4ad04275bb0a552a1a34c5fd3bc755
branchwag <branchleannelmb@gmail.com>
2025-08-10 19:14:33 -0400

    adding in array index testing sample

---
+++ array_access/target/debug/array_access.d
@@ -0,0 +1,1 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/array_access/target/debug/array_access: /home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/array_access/src/main.rs
---
+++ array_access/target/debug/deps/array_access-f592159594f3c815.d
@@ -0,0 +1,5 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/array_access/target/debug/deps/array_access-f592159594f3c815.d: src/main.rs
+
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/array_access/target/debug/deps/array_access-f592159594f3c815: src/main.rs
+
+src/main.rs:
