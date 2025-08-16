b00e35376c81136f721714cdb95c696b3e5d60ce
branchwag <branchleannelmb@gmail.com>
2025-08-16 19:10:28 -0400

    adding in quicksort

--- other_shenanigans/quicksort/src/main.rs
+++ other_shenanigans/quicksort/src/main.rs
@@ -1,26 +1,10 @@
-fn quicksort(arr: &mut [i8]) {
-    if arr.len() <= 1 {
-        return;
-    }
-    qsort(arr, 0, arr.len() - 1);
-}
-
-fn qsort(arr: &mut [i8], first: usize, last: usize) {
-    let split = partition(arr, first, last);
-    
-    if first < split {
-        qsort(arr, first, split);
-    }
-
-    if last > split + 1 {
-        qsort(arr, split + 1, last);
-    }
-}
-
-fn partition(arr: &mut [i8], first: usize, last: usize) -> usize {
+fn partition(arr: &mut [u8], first: usize, last:usize) -> usize {
     let pivot = arr[(first + last) / 2];
-    let mut i = first.wrapping_sub(1); //handle underflow
-    let mut j = last + 1;
+    let mut i = first;
+    let mut j = last;
+
+    i = i.wrapping_sub(1);
+    j = j.wrapping_add(1);
 
     loop {
         loop {
@@ -45,9 +29,38 @@ fn partition(arr: &mut [i8], first: usize, last: usize) -> usize {
     }
 }
 
+fn q_sort(arr: &mut [u8], first: usize, last: usize) {
+    if first >= last {
+        return;
+    }
+
+    let split = partition(arr, first, last);
+
+    if first < split {
+        q_sort(arr, first, split);
+    }
+
+    if last > split + 1 {
+        q_sort(arr, split + 1, last);
+    }
+}
+
+fn quicksort(arr: &mut [u8]) {
+    if arr.len() <= 1 {
+        return;
+    }
+    q_sort(arr, 0, arr.len() - 1);
+}
+
 fn main() {
-    let mut arr1 = [64, 34, 25, 12, 22, 11, 90];
-    println!("Original array: {:?}", arr1);
-    quicksort(&mut arr1);
-    println!("Sorted array: {:?}", arr1);
+
+    let mut arr: [u8; 5] = [5, 8, 2, 6, 1];
+
+    println!("Get ready to quicksort...");
+
+    println!("Here is the original array:");
+    println!("{:?}", arr);
+    quicksort(&mut arr);
+    println!("Here is our array after sorting:");
+    println!("{:?}", arr);
 }
