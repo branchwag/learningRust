8ccc53ade407e1d2ffaeaae6df87cb2a7ccf539f
branchwag <branchleannelmb@gmail.com>
2025-08-18 19:49:51 -0400

    adds brace checker

---
+++ other_shenanigans/check_braces/target/debug/check_braces.d
@@ -0,0 +1,1 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/other_shenanigans/check_braces/target/debug/check_braces: /home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/other_shenanigans/check_braces/src/main.rs
---
+++ other_shenanigans/check_braces/target/debug/deps/check_braces-cacbd734522a5656.d
@@ -0,0 +1,5 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/other_shenanigans/check_braces/target/debug/deps/check_braces-cacbd734522a5656.d: src/main.rs
+
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/other_shenanigans/check_braces/target/debug/deps/check_braces-cacbd734522a5656: src/main.rs
+
+src/main.rs:
