67fc5ed45db23bc8096b25a97ef8085f1b4bea4d
branchwag <branchleannelmb@gmail.com>
2025-08-18 19:49:51 -0400

    adds brace checker

---
+++ other_shenanigans/check_braces/src/main.rs
@@ -0,0 +1,31 @@
+fn valid_braces(s: &str) -> bool {
+    let mut stack = Vec::new();
+    
+    for ch in s.chars() {
+        match ch {
+            '(' | '[' | '{' => stack.push(ch),
+            ')' =>  {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            },
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            },
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            },
+            _ => return false,
+        }
+    }
+
+    stack.is_empty()
+}
+
+fn main() {
+    println!("{}", valid_braces("()"));
+}
