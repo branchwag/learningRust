944b2f8884f28985525c3437ae1ac492687f33b5
branchwag <branchleannelmb@gmail.com>
2025-08-22 19:20:50 -0400

    adding in borrowing exercises

---
+++ modding_borrowed/target/debug/deps/modding_borrowed-1b95cc99edde712c.d
@@ -0,0 +1,5 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/modding_borrowed/target/debug/deps/modding_borrowed-1b95cc99edde712c.d: src/main.rs
+
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/modding_borrowed/target/debug/deps/modding_borrowed-1b95cc99edde712c: src/main.rs
+
+src/main.rs:
