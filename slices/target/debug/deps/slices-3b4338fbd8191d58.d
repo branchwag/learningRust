16edaab1e98959223e474d37dcd272edc5e8b43c
branchwag <branchleannelmb@gmail.com>
2025-08-24 17:45:25 -0400

    adjusts slices example to return first word rather than length of first word

---
+++ slices/target/debug/deps/slices-3b4338fbd8191d58.d
@@ -0,0 +1,5 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/slices/target/debug/deps/slices-3b4338fbd8191d58.d: src/main.rs
+
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/slices/target/debug/deps/slices-3b4338fbd8191d58: src/main.rs
+
+src/main.rs:
---
+++ slices/target/debug/slices.d
@@ -0,0 +1,1 @@
+/home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/slices/target/debug/slices: /home/whiterabbit/CodingStuff/Rustin/the_rust_programming_language/slices/src/main.rs
