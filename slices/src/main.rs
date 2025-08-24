5dbcf86497499bad0d6df29b5d02aa349ddc4fb2
branchwag <branchleannelmb@gmail.com>
2025-08-24 17:45:25 -0400

    adjusts slices example to return first word rather than length of first word

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,17 +1,18 @@
 //take string of words separated by spaces
 //return first word
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world!");
+    println!("{}", first_word(&s))
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
     
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
