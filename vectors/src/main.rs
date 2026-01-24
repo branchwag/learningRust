54cd445a6116d4aaf0bc4802ec5bc239994bed52
branchwag <branchleannelmb@gmail.com>
2026-01-24 17:43:36 -0500

    adds summing vector experiment

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    println!("{:?}", v);
+    //making a vec and pushing
+    //let mut v: Vec<i32> = Vec::new();
+    //v.push(5);
+    //v.push(6);
+    //v.push(7);
+    // println!("{:?}", v);
+
+    //summing
+    let numbers = vec![1, 2, 3, 4, 5];
+    let sum: i32 = numbers.iter().sum();
+    println!("Sum: {}", sum);
+    println!("Vec : {:?}", numbers);
 }
