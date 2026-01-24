6f16275bea01f8be6950fbf52ce1f15b329f1212
branchwag <branchleannelmb@gmail.com>
2026-01-24 09:37:28 -0500

    adds lil http server

---
+++ other_shenanigans/http_server/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::{net::TcpListener, thread};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    if let Ok(_) = stream.read(&mut buffer) {
+        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
+        let _ = stream.write(response.as_bytes());
+        let _ = stream.flush();
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("Listening on port 3000...");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+}
