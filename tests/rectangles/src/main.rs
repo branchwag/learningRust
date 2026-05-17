e1c72fbfea536f9c4dd4c1060b3f5f2ad1419446
branchwag <branchleannelmb@gmail.com>
2026-05-17 09:01:28 -0400

    finishes setting up tests for rectangle example

--- tests/rectangles/src/main.rs
+++ tests/rectangles/src/main.rs
@@ -1,18 +1,4 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
+use rectangles::Rectangle;
 
 fn main() {
     let rect1 = Rectangle {
