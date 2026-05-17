3971e68de882e8b19e673f751fba2270405f3117
branchwag <branchleannelmb@gmail.com>
2026-05-17 08:49:25 -0400

    more work on the rectangle example

--- tests/rectangles/src/main.rs
+++ tests/rectangles/src/main.rs
@@ -4,20 +4,36 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 100,
+        height: 60,
+    };
+
     println!("rect1 is {rect1:?}");
+    println!("rect2 is {rect2:?}");
+
+    println!("The area of rectangle 1 is {} square pixels.", rect1.area());
+
+    println!("The area of rectangle 2 is {} square pixels.", rect2.area());
 
     println!(
-        "The area of the recntangle is {} square pixels.",
-        area(&rect1)
+        "Will the second rectangle hold the first rectangle? {}",
+        rect2.can_hold(&rect1)
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
