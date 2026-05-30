6b1abdbb341e2ad03520a2524ff6a4bfd91a6d34
branchwag <branchleannelmb@gmail.com>
2026-05-30 11:47:57 -0400

    adding custom Box example

---
+++ smart_pointers/my_box/src/main.rs
@@ -0,0 +1,25 @@
+struct MyBox<T>(T);
+//to implement a trait, we need to provide implementations for the trais's required methods
+use std::ops::Deref;
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
