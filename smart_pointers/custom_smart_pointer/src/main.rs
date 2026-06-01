9147cde6e13720c89971cc7fb98ebaeaded72a71
branchwag <branchleannelmb@gmail.com>
2026-06-01 19:22:49 -0400

    adding dropping example

--- smart_pointers/custom_smart_pointer/src/main.rs
+++ smart_pointers/custom_smart_pointer/src/main.rs
@@ -16,4 +16,10 @@ fn main() {
         data: String::from("other stuff"),
     };
     println!("CustomSmartPointers created");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main");
 } //dont need to call drop method explicitly
+//
+//variables are dropped in reverse order of their creation
+//
+//use std::mem::drop to force drop a value before the end of its scope
