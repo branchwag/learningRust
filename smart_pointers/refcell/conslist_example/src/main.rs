996415b3bb129ca873f881e42701a4c5b3e4aa95
branchwag <branchleannelmb@gmail.com>
2026-06-02 20:09:21 -0400

    adds rcrefcell example

---
+++ smart_pointers/refcell/conslist_example/src/main.rs
@@ -0,0 +1,29 @@
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+//Rc - multiple owners of the same data, usually immutable
+//RefCell - for its ability to change values in the lists
+//
+//so using an Rc that holds a RefCell, you can get a value that can have multiple owners and you can
+//mutate
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    // b and c both refer to a
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
+}
