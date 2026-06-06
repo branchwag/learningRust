dd87b8cb2b9cc40e8e65aedc65541f6b8a7330a7
LeAnne B. <branchleannelmb@gmail.com>
2026-06-05 23:08:30 -0400

    adding example of transferring data between threads

---
+++ fearless_concurrency/transferring_data_between_threads/src/main.rs
@@ -0,0 +1,21 @@
+use std::sync::mpsc;
+//mpsc - multiple producer, single consumer
+//channels can have multiple sending ends that produce values
+//only one receiving end that consumes those values
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    //tx - transmitter
+    //rx - receiver
+
+    thread::spawn(move || { //using move to move tx into the closure
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        //send method returns Result<T, E> 
+        //upwrap here will panic in case of an error
+
+});
+        let received = rx.recv().unwrap();
+        println!("Got: {received}");
+}
