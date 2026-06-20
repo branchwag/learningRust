5766d546f1d24014e0587664f8ff4a8af72686e1
branchwag <branchleannelmb@gmail.com>
2026-06-20 11:55:10 -0400

    updating aysnc example to use awat with join handle to run task to completion

--- async_programming/hello-async/src/main.rs
+++ async_programming/hello-async/src/main.rs
@@ -2,17 +2,18 @@ use std::time::Duration;
 
 fn main() {
     trpl::block_on(async {
-        //polls future
-        trpl::spawn_task(async {
+        let handle = trpl::spawn_task(async {
             for i in 1..10 {
                 println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await; //let other tasks run
+                trpl::sleep(Duration::from_millis(500)).await;
             }
-        }); //creating a new task to print one thing while the main task prints something else
+        });
 
         for i in 1..5 {
             println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await; //let other tasks run
+            trpl::sleep(Duration::from_millis(500)).await;
         }
+
+        handle.await.unwrap();
     });
 }
