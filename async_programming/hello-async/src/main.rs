889dc79da2f1729326cacab4d640d4325702a09d
branchwag <branchleannelmb@gmail.com>
2026-06-20 12:46:12 -0400

    sending data between two tasks using message passing

--- async_programming/hello-async/src/main.rs
+++ async_programming/hello-async/src/main.rs
@@ -1,19 +1,11 @@
-use std::time::Duration;
-
 fn main() {
     trpl::block_on(async {
-        let handle = trpl::spawn_task(async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        });
+        let (tx, mut rx) = trpl::channel();
 
-        for i in 1..5 {
-            println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let val = String::from("hi");
+        tx.send(val).unwrap();
 
-        handle.await.unwrap();
+        let received = rx.recv().await.unwrap();
+        println!("received '{received}'");
     });
 }
