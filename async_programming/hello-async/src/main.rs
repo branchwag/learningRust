549f32ca260717e853d57215fbac253c65674096
branchwag <branchleannelmb@gmail.com>
2026-06-20 13:25:30 -0400

    sending and receiving multiple messages over the async channel and sleeping with an await between each message

--- async_programming/hello-async/src/main.rs
+++ async_programming/hello-async/src/main.rs
@@ -1,11 +1,23 @@
+use std::time::Duration;
+
 fn main() {
     trpl::block_on(async {
         let (tx, mut rx) = trpl::channel();
 
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("future"),
+        ];
 
-        let received = rx.recv().await.unwrap();
-        println!("received '{received}'");
+        for val in vals {
+            tx.send(val).unwrap();
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        while let Some(value) = rx.recv().await {
+            println!("received '{value}'");
+        }
     });
 }
