4bbea84e8e9ad5b36cdb4cec932b26cf724ede7a
branchwag <branchleannelmb@gmail.com>
2026-06-20 11:38:57 -0400

    adding tasks with await

--- async_programming/hello-async/src/main.rs
+++ async_programming/hello-async/src/main.rs
@@ -1,31 +1,18 @@
-use trpl::{Either, Html};
-
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let response_text = trpl::get(url).await.text().await;
-    let title = Html::parse(&response_text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
-    //we get an Option back so we are using Option::map
-    //method.
-}
+use std::time::Duration;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
     trpl::block_on(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
+        //polls future
+        trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await; //let other tasks run
+            }
+        }); //creating a new task to print one thing while the main task prints something else
 
-        let (url, maybe_title) = match trpl::select(title_fut_1, title_fut_2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
-        };
-
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title was {title}"),
-            None => println!("It had no title."),
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await; //let other tasks run
         }
-    })
+    });
 }
