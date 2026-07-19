0309a1aa4e382ca1e1b8230a8a9832257184d852
branchwag <branchleannelmb@gmail.com>
2026-07-19 17:02:44 -0400

    final project - serves html only at /

--- final_project/hello/src/main.rs
+++ final_project/hello/src/main.rs
@@ -6,19 +6,19 @@ use std::{
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream); //ref to stream
-    let http_request: Vec<_> = buf_reader //collect what browser sends to out server
-        .lines() //returns iterator of Result<String, std::io::Error> by splitting the stream of data whenever it sees a newline byte
-        .map(|result| result.unwrap()) //getting each String
-        .take_while(|line| !line.is_empty()) //end of hhtp = 2 newline chars
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        //some other request
+    }
 }
 
 fn main() {
