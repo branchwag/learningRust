272d7966987147e182c0128c32db08363e5bf595
branchwag <branchleannelmb@gmail.com>
2026-07-19 16:21:59 -0400

    final project - gets response working

--- final_project/hello/src/main.rs
+++ final_project/hello/src/main.rs
@@ -1,11 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, prelude::*}, //read-from write-to stream
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream); //ref to stream
+    let http_request: Vec<_> = buf_reader //collect what browser sends to out server
+        .lines() //returns iterator of Result<String, std::io::Error> by splitting the stream of data whenever it sees a newline byte
+        .map(|result| result.unwrap()) //getting each String
+        .take_while(|line| !line.is_empty()) //end of hhtp = 2 newline chars
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap()
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established!");
+        handle_connection(stream);
     }
 }
